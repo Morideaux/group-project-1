fed4e5d444e5a580cd58efe54db18ec568630bab
austinseger <austin.seger.1024@gmail.com>
2024-04-11 20:31:37 -0700

    calling apis and html script

---
+++ Assets/api.js
@@ -0,0 +1,179 @@
+fetch('https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('movie questions easy \n----------');
+    console.log(data);
+  });
+
+    fetch('https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('movie questions medium \n----------');
+    console.log(data);
+  });
+
+    fetch('https://opentdb.com/api.php?amount=5&category=11&difficulty=hard&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('movie questions hard \n----------');
+    console.log(data);
+  });
+
+    fetch('https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('geo questions easy \n----------');
+    console.log(data);
+  });
+
+    fetch('https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('geo questions medium \n----------');
+    console.log(data);
+  });
+
+    fetch('https://opentdb.com/api.php?amount=5&category=22&difficulty=hard&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('geo questions hard \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=27&difficulty=hard&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('animals questions hard \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=27&difficulty=medium&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('animals questions medium \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('animals questions easy \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=hard&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('history questions hard \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=medium&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('history questions medium \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('history questions easy \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=hard&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('general questions hard \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('general questions medium \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('general questions easy \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=17&difficulty=hard&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('sci and nat questions hard \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=17&difficulty=medium&type=multiple')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('sci and nat questions medium \n----------');
+    console.log(data);
+  });
+
+  fetch('https://opentdb.com/api.php?amount=5&category=17&difficulty=easy&type=boolean')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('sci and nat questions easy \n----------');
+    console.log(data);
+  });
+
+async function parallelCall() {
+    
+    let start_time = new Date().getTime();
+    let promises = [];
+    let result = [];
+  
+    let currentPage = 1;
+  
+    while (currentPage <= 5) {
+      promises.push(fakeApiCall(currentPage));
+      currentPage++;
+    }
+  
+    const data = await Promise.all(promises);
+    data.forEach(({ data }) => {
+      result = [result, data];
+    })}
