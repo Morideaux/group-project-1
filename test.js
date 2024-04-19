30989851bcc2656002297a18d1ab84449cf766dd
myname <you@example.com>
2024-04-18 19:46:47 -0700

    fixed styles

--- test.js
+++ test.js
@@ -13,16 +13,16 @@ async function sendAPiRequest(){
 
 function useApiData(data){
     document.querySelector("#questionSlot").innerHTML =  `Question: ${data.results[0].question}`
-    document.querySelector("#answer1").innerHTML =  data.results[0].corect_answer
-    document.querySelector("#answer2").innerHTML =  data.results[0].incorrect_answer[0]
-    document.querySelector("#answer3").innerHTML =  data.results[0].incorrect_answer[1]
-    document.querySelector("#answer4").innerHTML =  data.results[0].incorrect_answer[2]
+    document.querySelector("#answer1").innerHTML =  data.results[0].correct_answer
+    document.querySelector("#answer2").innerHTML =  data.results[0].incorrect_answers[0]
+    document.querySelector("#answer3").innerHTML =  data.results[0].incorrect_answers[1]
+    document.querySelector("#answer4").innerHTML =  data.results[0].incorrect_answers[2]
     
 }
 
 let correctButton = document.querySelector('answer1')
 
-correctButton.addEventListener('click',()=>{
+correctButton.addEventListener("click",()=>{
     alert("Correct")
     sendAPiRequest
-});
+})
